814f5f813d8e3c743d1358234d6020f7d078ce1b
idow09 <idow09@gmail.com>
2020-07-14 23:52:33 +0300

    UI of arXiv is now updated!
    Added the UI callback for updating the list

--- content_arxiv.js
+++ content_arxiv.js
@@ -1,5 +1,9 @@
 var paper_title = document.querySelector("#abs h1").childNodes[1].textContent;
 chrome.runtime.sendMessage({title: paper_title}, function(response) {
-	console.log(response.txt);
-	console.log(response.paper_link);
+	var a = document.createElement('a');
+	a.innerText = response.txt;
+	a.href = response.paper_link;
+	var li = document.createElement('li');
+	li.appendChild(a);
+	document.querySelector(".full-text ul").appendChild(li);
 });
