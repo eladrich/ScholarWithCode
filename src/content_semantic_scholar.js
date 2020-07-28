4fd043e38296eb9ee322029e6c0999fd6199275f
idow09 <idow09@gmail.com>
2020-07-29 00:00:05 +0300

    Refactored and beutified

--- src/content_semantic_scholar.js
+++ src/content_semantic_scholar.js
@@ -1,3 +1,21 @@
+function makeIconElement() {
+    let xmlns = 'http://www.w3.org/2000/svg';
+    let svgElem = document.createElementNS(xmlns, "svg");
+    let iconSize = 18;
+    let viewSize = 432 / iconSize;
+    svgElem.setAttributeNS(null, "viewBox", "0 0 " + viewSize + " " + viewSize);
+    svgElem.setAttributeNS(null, "width", iconSize);
+    svgElem.setAttributeNS(null, "height", iconSize);
+    let path1 = document.createElementNS(xmlns, "path");
+    path1.setAttributeNS(null, "d", "M0 0h24v24H0V0z");
+    path1.setAttributeNS(null, "fill", "none");
+    let path2 = document.createElementNS(xmlns, "path");
+    path2.setAttributeNS(null, "d", "M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z");
+    svgElem.appendChild(path1);
+    svgElem.appendChild(path2);
+    return svgElem;
+}
+
 let observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (!mutation.addedNodes) return
@@ -8,27 +26,16 @@ let observer = new MutationObserver((mutations) => {
             // Go over all regions and add code implementations
             for (let i = 0, l = results.length; i < l; i++) {
                 let title = results[i].childNodes[0].childNodes[0].textContent;
-                chrome.runtime.sendMessage({title: title, payload: i}, function(response) {
-                    let results = document.getElementsByClassName('paper-actions');				
-					let result = results[response.payload];
-					let a = result.firstChild.cloneNode(true);
-					let xmlns = 'http://www.w3.org/2000/svg';
-                    let svgElem = document.createElementNS(xmlns, "svg");
-					let iconSize = 18;
-					let viewSize = 432 / iconSize;
-                    svgElem.setAttributeNS(null, "viewBox", "0 0 " + viewSize + " " + viewSize);
-                    svgElem.setAttributeNS(null, "width", iconSize);
-                    svgElem.setAttributeNS(null, "height", iconSize);
-                    let path1 = document.createElementNS(xmlns,"path");
-                    path1.setAttributeNS(null, "d", "M0 0h24v24H0V0z");
-                    path1.setAttributeNS(null, "fill", "none");
-                    let path2 = document.createElementNS(xmlns,"path");
-                    path2.setAttributeNS(null, "d", "M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z");
-                    svgElem.appendChild(path1);
-                    svgElem.appendChild(path2);
-                    a.firstChild.replaceChild(svgElem, a.firstChild.firstChild);
-					a.firstChild.childNodes[1].innerText = response.txt;
+                chrome.runtime.sendMessage({
+                    title: title,
+                    payload: i
+                }, function(response) {
+                    let results = document.getElementsByClassName('paper-actions');
+                    let result = results[response.payload];
+                    let a = result.firstChild.cloneNode(true);
                     a.href = response.paper_link;
+                    a.firstChild.replaceChild(makeIconElement(), a.firstChild.firstChild);
+                    a.firstChild.childNodes[1].innerText = response.txt;
                     result.appendChild(a);
                 });
             }
